1e7d0d58db773b8945adc6a2d4cfd6688db2e407
XingCheng901024 <1257060187@qq.com>
2020-03-04 23:17:10 +0800

    init

---
+++ base-eureka2/src/test/java/com/xc/baseeureka2/BaseEureka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xc.baseeureka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseEureka2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
