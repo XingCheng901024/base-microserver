4990750cd1fc9352fec4c5bd44b8c6c4d156c935
XingCheng901024 <1257060187@qq.com>
2020-03-09 23:57:33 +0800

    eureka+docker-compose

---
+++ docker-eureka-cluster/src/test/java/com/xc/dockereurekacluster/DockerEurekaClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xc.dockereurekacluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEurekaClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
